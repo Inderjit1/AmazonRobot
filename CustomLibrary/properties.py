631c2b2651ba7403b3febf9a8235a40143c2adad
Inderjit1 <inderjitbassi1@gmail.com>
2021-01-21 11:11:29 -0800

    Update properties.py

--- CustomLibrary/properties.py
+++ CustomLibrary/properties.py
@@ -1,26 +1,25 @@
-USERNAME = 'inbass92'
-PASSWORD = 'Hahaha123!'
-ID = '010203453'
-SECRET_SERVER_PWD = "Boxing2015!"
-SECRET_SERVER_DB = "CASJDVL"
+USERNAME = 'XXXXXXXX'
+PASSWORD = 'XXXXXXXX'
+ID = 'XXXXXXXX'
+SECRET_SERVER_PWD = "XXXXXXXX"
+SECRET_SERVER_DB = "XXXXXXXX"
 
-DELPHIXURL = 'https://delphix801.cms.calstate.edu/login/index.html?from=%2FServer.html'
-UNIWEBURL = 'https://uniweb20.calstate.edu/login'
-TSSURL    = 'https://secretserver.sjsu.edu/Login.aspx?ReturnUrl=%2f'
+DELPHIXURL = 'XXXXXXXX'
+UNIWEBURL = 'XXXXXXXX'
+TSSURL    = 'XXXXXXXX'
 
-TARGET_DB = "casjprj"
-SOURCE_DB = "csjprd"
+TARGET_DB = "XXXXXXXX"
+SOURCE_DB = "XXXXXXXX"
 
-# CS - xpath=//*[@id="test-l-1"]/div[2]/div[1]/div/span/input, HR - xpath=//*[@id="test-l-1"]/div[2]/div[2]/div/span/input
-CS_OR_HR_SELECT = 'xpath=//*[@id="test-l-1"]/div[2]/div[1]/div/span/input'
 
-APP_DB = 'csjappdev.cms.calstate.edu'
-DEV_DB = 'csjdev700.sldc.calstate.edu'
-APPSERVER_STOP_SCRIPT = 'sudo ps_admin –d cbsjprj –o stop_all'
-CHANGE_PASSWORD_SCRIPT = 'sudo db_mgr –d cbsjprj –o chg_pwd'
-NEW_SYSADM_PWD = 'Sugardy1'
-APPSERVER_START_SCRIPT = 'sudo ps_admin –d cbsjprj –o clr_cache'
+CS_OR_HR_SELECT = 'XXXXXXXX'
 
-SQL_PLUS_USERNAME_PASSWORD = 'SYSADM@CASJSTG/Sugardy3'
-SQL_PLUS_SCRIPT_PATH = "@C:\\Users\\010203453\\Documents\\Post Clone Scripts\\CS\\casjstg_clone.sql"
+APP_DB = 'XXXXX'
+DEV_DB = 'XXXXX'
+APPSERVER_STOP_SCRIPT = 'XXX'
+CHANGE_PASSWORD_SCRIPT = 'XXXX'
+NEW_SYSADM_PWD = 'XXXXX'
+APPSERVER_START_SCRIPT = 'XXXXX'
 
+SQL_PLUS_USERNAME_PASSWORD = 'XXXXX'
+SQL_PLUS_SCRIPT_PATH = "XXXXX"
